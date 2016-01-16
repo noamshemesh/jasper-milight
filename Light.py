b2f954d42af6553a3484647d413cd0a4f220264a
Noam Shemesh <noam@bigpanda.io>
2016-01-16 14:44:08 +0200

    first commit

---
+++ Light.py
@@ -0,0 +1,67 @@
+import milight
+
+PRIORITY = 10
+WORDS = ["LIGHT", "LIGHTS", "ON", "OFF", "DIM", "WHITE", "FIRST", "SECOND", "THIRD", "FOURTH", "ALL"]
+
+template = re.compile(r'\(all|first|second|third|fourth)? ?lights? (on|off|white|dim)\b', re.IGNORECASE)
+words_to_numbers = {
+    'all': 0
+    'first': 1
+    'second': 2
+    'third': 3
+    'forth': 4
+}
+
+
+def isValid(text):
+    return bool(template.search(text))
+
+
+def message(text, command, group):
+    message = ''
+    if command == 'dim':
+        message += 'Dimming '
+    elif command == 'white':
+        message += 'Setting '
+    else:
+        message += 'Turning '
+
+    if group == 'all':
+        message += 'all lights '
+    else:
+        message += group + ' group '
+
+    if command != 'dim':
+        if command == 'white':
+            message += 'to white'
+        else:
+            message += command
+
+    return message
+
+
+def handle(text, mic, profile):
+    ip = profile['milight']['ip']
+    port = profile['milight']['port'] or 8899
+    wait_duration = profile['milight']['wait_duration'] or 200
+    bulb_type = profile['milight']['bulb_type'] or ['rgbw']
+    controller = milight.MiLight({'host': ip, 'port': port}, wait_duration=wait_duration)
+    light = milight.LightBulb(bulb_type)
+
+    m = template.search(text)
+    light_group_string = m.group(0)
+    command = m.group(1).lower()
+
+    if !light_group_string:
+        light_group_string = 'all'
+    light_group_string = light_group_string.lower()
+    light_group_int = words_to_numbers[light_group_string]
+
+    try:
+        if command == 'dim':
+            controller.send(light.brightness(50, light_group_int))
+        else:
+            controller.send(light[command](light_group_string))
+        mic.say(message(text, command, light_group_string))
+    except:
+        mic.say('Fail to send command to lights')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jasper-milight
-Jasper module to turn on, turn off and dim your lights
+Jasper module to turn on, turn off, set to white and dim your lights. supporting all 4 groups
+
+### INSTALLATION
+1. clone this repository to a machine with [jasper](http://jasperproject.github.io/) installed
+1. run `pip install -r /path/to/requirements.txt` to install requirements
+1. copy `Light.py` to `$JASPER_HOME/client/modules` (replace $JASPER_HOME with jasper home if not set)
